406359ffc1ac5531eca23b0db68fc86710fff0c8
Sifatul Rabbi <mdsifatulislam.rabbi@gmail.com>
2025-05-13 09:23:35 +0600

    testing large text

--- text_to_unicode_and_reverse/text_to_unicode_and_reverse.go
+++ text_to_unicode_and_reverse/text_to_unicode_and_reverse.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -8,24 +9,52 @@ import (
 	"strings"
 )
 
+func getTestInput() string {
+	f, err := os.Open("./test_input.txt")
+	if err != nil {
+		log.Fatalln("Unable to find the file\n", err)
+	}
+	buff := make([]byte, 1024)
+	if _, err = f.Read(buff); err != nil {
+		log.Fatalln("Unable to read the file\n", err)
+	}
+	return bytes.NewBuffer(buff).String()
+}
+
 func main() {
 	args := os.Args[1:]
 	if len(args) < 1 {
 		log.Fatalln("Please provide a text and the type")
 	}
 
-	unicodeInput := args[0] == "-u"
-
-	if !unicodeInput {
-		for _, r := range strings.Join(args[0:], " ") {
-			fmt.Printf("%d ", r)
-		}
-		fmt.Println()
-	} else {
-		for _, strRuneCode := range args[1:] {
-			runeCode, _ := strconv.ParseInt(strRuneCode, 10, 32)
-			fmt.Printf("%c", runeCode)
-		}
-		fmt.Println()
+	var result string
+	switch args[0] {
+	case "test":
+		result = strToUnicode(strings.Split(getTestInput(), " "))
+		break
+	case "-u":
+		result = unicodeToStr(args[1:])
+		break
+	default:
+		result = strToUnicode(args[0:])
+		break
 	}
+	fmt.Println(result)
+}
+
+func strToUnicode(text []string) string {
+	result := ""
+	for _, r := range strings.Join(text, " ") {
+		result += fmt.Sprintf("%d ", r)
+	}
+	return result
+}
+
+func unicodeToStr(unicodeText []string) string {
+	result := ""
+	for _, strRuneCode := range unicodeText {
+		runeCode, _ := strconv.ParseInt(strRuneCode, 10, 32)
+		result += fmt.Sprintf("%c", runeCode)
+	}
+	return result
 }
